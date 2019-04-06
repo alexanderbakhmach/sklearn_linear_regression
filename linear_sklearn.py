6002b388e769ae4e47994c08548632bf5152fcd2
Alexander <alexandrbakhmach@gmail.com>
2019-04-06 13:25:24 +0300

    Change dataset

--- linear_sklearn.py
+++ linear_sklearn.py
@@ -1,21 +1,18 @@
-import numpy 
+import numpy as np
 import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plot
+from sklearn.metrics import r2_score
 
 
-"""
- Sklearn linear regression example with kaggle dataset 
- Dataset: https://www.kaggle.com/sohier/calcofi
-"""
-
-DATASET_PATH = 'bottle.csv'
-RANGE = 0.4
+DATASET_PATH = 'data.csv'
+RANGE = 0.1
 
 # Read scv
 print('Reading data ...')
-df = pandas.read_csv(DATASET_PATH, usecols=['Salnty', 'T_degC'])
+df = pandas.read_csv(DATASET_PATH, usecols=['Temperature (C)', 'Humidity'])
+df.columns = ['hum', 'temp']
 
 # Print the firt data in dataset
 head = df.head()
@@ -25,8 +22,8 @@ print(head)
 df = df.dropna()
 
 # Devide data for features and labels
-features = df.Salnty.values.reshape((-1, 1))
-labels = df.T_degC.values
+labels = df.hum.values
+features = df.temp.values.reshape((-1, 1))
 
 # Get train and test set
 x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=RANGE)
@@ -40,3 +37,14 @@ model.fit(x_train, y_train)
 # Get predictions
 train_pred = model.predict(x_train)
 test_pred = model.predict(x_test)
+
+print('coef - {} , inter - {}'.format(model.coef_, model.intercept_))
+
+# Visualize data
+plot.scatter(x_test, y_test, color = 'blue')
+plot.plot(x_test, test_pred)
+plot.title('Dependancy between salnty and temperature')
+plot.xlabel('Salnty')
+plot.ylabel('Temperature')
+plot.show()
+
